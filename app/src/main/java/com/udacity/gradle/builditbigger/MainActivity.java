8f2ba441f4d111cdda219900b9b392d898009587
Mohammad AL-Ali <m.alali.sy@gmail.com>
2016-08-29 00:48:50 +0300

    chore: Add the GCE backend
    * Create a Google Cloud Endpoint to get jokes from JokesProvider Java library.
    
    * Add dependency between the GCE Module and the Java library.
    
    * Include the library in the settings.gradle in the main project and add the dependency of the library project in the app build.gradle.
    
    * Use the GCE Module in AsyncTask to pass a joke to the JokesViewer Android library.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,22 +1,38 @@
 package com.udacity.gradle.builditbigger;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.shamdroid.JokesProvider;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import com.shamdroid.backend.myApi.MyApi;
 import com.shamdroid.jokesviewrlibrary.JokeViewerActivity;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 
@@ -43,10 +59,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-//        Toast.makeText(this, JokesProvider.tellMeJoke(), Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(MainActivity.this , JokeViewerActivity.class);
-        intent.putExtra("joke" , JokesProvider.tellMeJoke());
-        startActivity(intent);
+        new MyAsyncTask().execute(this);
+    }
+
+
+
+    static class MyAsyncTask extends AsyncTask<Context,Void,String>{
+        static MyApi myApiService;
+        Context context ;
+        ProgressDialog progressDialog;
+
+        @Override
+        protected String doInBackground(Context... contexts) {
+
+            context = contexts[0];
+
+
+
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    progressDialog = new ProgressDialog(context);
+                    progressDialog.setMessage(context.getString(R.string.pleaseWait));
+                    progressDialog.show();
+                }
+            });
+
+            if (myApiService == null ){
+
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+
+            try {
+                return myApiService.tellJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+
+        }
+
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            progressDialog.dismiss();
+            Intent intent = new Intent(context , JokeViewerActivity.class);
+            intent.putExtra("joke",s);
+            context.startActivity(intent);
+        }
     }
 
 
---
+++ backend/src/main/java/com/shamdroid/backend/MyEndpoint.java
@@ -0,0 +1,38 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.shamdroid.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.shamdroid.JokesProvider;
+
+import javax.inject.Named;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.shamdroid.com",
+                ownerName = "backend.shamdroid.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+
+    @ApiMethod(name = "tellJoke")
+    public MyBean sayHi() {
+        MyBean response = new MyBean();
+        response.setData(JokesProvider.tellMeJoke());
+        return response;
+    }
+
+}
